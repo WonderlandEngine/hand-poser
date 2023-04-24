4eed216d776b5a9c5c71c9d7246ab6d21b81941c
NSTCG <nithinsteven32@gmail.com>
2023-04-24 16:24:07 +0200

    Migrate HandPoser to 1.0.0

--- js/button.js
+++ js/button.js
@@ -1,28 +1,46 @@
-require('@wonderlandengine/components/howler-audio-source');
-require('@wonderlandengine/components/howler-audio-listener');
+import {Component, MeshComponent, Type} from '@wonderlandengine/api';
+import {CursorTarget, HowlerAudioSource} from '@wonderlandengine/components';
 
-WL.registerComponent('button', {
-    hoverMaterial: {type: WL.Type.Material},
-}, {
-    start: function() {
-        this.mesh = this.object.getComponent('mesh');
+export class Button extends Component {
+    static TypeName = 'button';
+    static Properties = {
+        hoverMaterial: {type: Type.Material},
+    };
+    static onRegister(engine) {
+        engine.registerComponent(HowlerAudioSource);
+    }
+
+    start() {
+        this.mesh = this.object.getComponent(MeshComponent);
         this.defaultMaterial = this.mesh.material;
+        this.target = this.object.getComponent(CursorTarget);
+        this.target.onHover.add(this.onHover.bind(this));
+        this.target.onUnhover.add(this.onUnHover.bind(this));
+        this.target.onDown.add(this.onDown.bind(this));
+        this.target.onUp.add(this.onUp.bind(this));
 
-        this.target = this.object.getComponent('cursor-target');
-        this.target.addHoverFunction(this.onHover.bind(this));
-        this.target.addUnHoverFunction(this.onUnHover.bind(this));
+        this.soundClick = this.object.addComponent(HowlerAudioSource, {
+            src: 'sfx/click.wav',
+            spatial: true,
+        });
+        this.soundUnClick = this.object.addComponent(HowlerAudioSource, {
+            src: 'sfx/unclick.wav',
+            spatial: true,
+        });
+    }
 
-        this.soundClick = this.object.addComponent('howler-audio-source', {src: 'sfx/click.wav', spatial: true});
-        this.soundUnClick = this.object.addComponent('howler-audio-source', {src: 'sfx/unclick.wav', spatial: true});
-    },
-
-    onHover: function() {
+    onHover() {
         this.mesh.material = this.hoverMaterial;
-        this.soundClick.play();
-    },
+    }
 
-    onUnHover: function() {
+    onUnHover() {
         this.mesh.material = this.defaultMaterial;
+    }
+
+    onDown() {
+        this.soundClick.play();
+    }
+    onUp() {
         this.soundUnClick.play();
-    },
-});
+    }
+}
--- js/hand-poser.js
+++ js/hand-poser.js
@@ -1,3 +1,5 @@
+import {Component, Type} from '@wonderlandengine/api';
+
 import {quat2} from 'gl-matrix';
 
 function mirror(q) {
@@ -16,69 +18,87 @@ function mirror(q) {
     return q;
 }
 
-WL.registerComponent('hand-pose', {
-    jsonFile: {type: WL.Type.String, default: ''},
-}, {
-    ORDERED_JOINTS: [
-        "wrist",
-        "thumb-metacarpal", "thumb-phalanx-proximal", "thumb-phalanx-distal", "thumb-tip",
-        "index-finger-metacarpal", "index-finger-phalanx-proximal", "index-finger-phalanx-intermediate", "index-finger-phalanx-distal", "index-finger-tip",
-        "middle-finger-metacarpal", "middle-finger-phalanx-proximal", "middle-finger-phalanx-intermediate", "middle-finger-phalanx-distal", "middle-finger-tip",
-        "ring-finger-metacarpal", "ring-finger-phalanx-proximal", "ring-finger-phalanx-intermediate", "ring-finger-phalanx-distal", "ring-finger-tip",
-        "pinky-finger-metacarpal", "pinky-finger-phalanx-proximal", "pinky-finger-phalanx-intermediate", "pinky-finger-phalanx-distal", "pinky-finger-tip"
-    ],
+export class HandPose extends Component {
+    static TypeName = 'hand-pose';
+    static Properties = {
+        jsonFile: {type: Type.String, default: ''},
+    };
 
-    start: function() {
+    ORDERED_JOINTS = [
+        'wrist',
+        'thumb-metacarpal',
+        'thumb-phalanx-proximal',
+        'thumb-phalanx-distal',
+        'thumb-tip',
+        'index-finger-metacarpal',
+        'index-finger-phalanx-proximal',
+        'index-finger-phalanx-intermediate',
+        'index-finger-phalanx-distal',
+        'index-finger-tip',
+        'middle-finger-metacarpal',
+        'middle-finger-phalanx-proximal',
+        'middle-finger-phalanx-intermediate',
+        'middle-finger-phalanx-distal',
+        'middle-finger-tip',
+        'ring-finger-metacarpal',
+        'ring-finger-phalanx-proximal',
+        'ring-finger-phalanx-intermediate',
+        'ring-finger-phalanx-distal',
+        'ring-finger-tip',
+        'pinky-finger-metacarpal',
+        'pinky-finger-phalanx-proximal',
+        'pinky-finger-phalanx-intermediate',
+        'pinky-finger-phalanx-distal',
+        'pinky-finger-tip',
+    ];
+
+    start() {
         this.hand = this.object.getComponent('hand-tracking');
-        if(this.jsonFile != "")
-            fetch(this.jsonFile).then(function(d) { d.json(); }).then(this.onHandPoseDownloaded.bind(this));
-    },
+        if (this.jsonFile != '')
+            fetch(this.jsonFile)
+                .then(function (d) {
+                    d.json();
+                })
+                .then(this.onHandPoseDownloaded.bind(this));
+    }
 
-    mirrorPose: function(out, pose) {
-        for(let key in pose) out[key] = mirror(pose[key]);
+    mirrorPose(out, pose) {
+        for (let key in pose) out[key] = mirror(pose[key]);
         return out;
-    },
+    }
 
     /* Create and download a snapshot of the current hand pose */
-    snapshot: function() {
-        const children = this.object.children;
-
+    snapshot() {
         const snapshot = {};
-        for(let i = 0; i < children.length; ++i) {
-            const o = children[i];
+        for (const o of this.object.children) {
             const name = o.name;
             /* We only snapshot bone poses, not the mesh or other attachments */
-            if(!this.ORDERED_JOINTS.includes(name)) continue;
-            if(name.endsWith('wrist')) continue;
+            if (!this.ORDERED_JOINTS.includes(name)) continue;
+            if (name.endsWith('wrist')) continue;
 
             /* Have the snapshot independent of handedness, save right hand only */
-            snapshot[name.substr(4)] = Array.from(o.transformLocal);
+            snapshot[name.substr(4)] = o.getTransformLocal(new Float32Array(8));
         }
 
         /* If this is the left hand, we mirror to the right */
-        if(this.hand && this.hand.handedness == 'left') {
+        if (this.hand && this.hand.handedness == 'left') {
             this.mirrorPose(snapshot, snapshot);
         }
 
         return snapshot;
-    },
+    }
 
-    applyPose: function(pose) {
-        const children = this.object.children;
-
+    applyPose(pose) {
         /* If this is the left hand, we mirror to the right */
-        if(this.hand && this.hand.handedness == 'left') {
+        if (this.hand && this.hand.handedness == 'left') {
             pose = this.mirrorPose({}, pose);
         }
 
-        for(let i = 0; i < children.length; ++i) {
-            const o = children[i];
+        for (const o of this.object.children) {
             const name = o.name.substr(4);
-
-            if(!(name in pose)) continue;
-
+            if (!(name in pose)) continue;
             o.transformLocal.set(pose[name]);
             o.setDirty();
         }
-    },
-});
+    }
+}
